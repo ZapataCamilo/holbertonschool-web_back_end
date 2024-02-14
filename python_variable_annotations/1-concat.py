b7be628e32f26f6ee5d9db43e25a8455a8a023bf
ZapataCamilo <zapatacamilo86@gmail.com>
2024-02-14 06:08:56 -0500

    python_variable_annotations

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Has a single function 'concat', that
+takes 2 arguments and returns them
+joined together, assuming they're both strings.
+
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Returns 'str1' and 'str2',
+    joined as a single string.
+    """
+    return str1 + str2
