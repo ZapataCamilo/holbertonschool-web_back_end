83a93ea9925611bdee6f61abd9c3279a9ca3324a
ZapataCamilo <zapatacamilo86@gmail.com>
2024-02-14 06:01:16 -0500

    python_variable_annotations

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -9,4 +9,8 @@ takes float arguments and returns a float.
 
 
 def add(a: float, b: float) -> float:
+    """
+    'a', 'b' and the returned result
+    are assumed to be of type 'float'.
+    """
     return a + b
