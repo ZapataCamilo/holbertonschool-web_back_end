67b9c825903626a3a31d31c2924912d8456a297b
ZapataCamilo <zapatacamilo86@gmail.com>
2024-02-14 06:00:01 -0500

    python_variable_annotations

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
+"""
+Has function 'add', which
+takes 2 arguments, and returns their sum.
+
+Uses Python type hints to specify that 'add'
+takes float arguments and returns a float.
+"""
+
+
 def add(a: float, b: float) -> float:
     return a + b
