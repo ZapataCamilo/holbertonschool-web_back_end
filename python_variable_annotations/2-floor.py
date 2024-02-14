ee6f482b65708d8ac9ebe287adad1db1f01ac658
ZapataCamilo <zapatacamilo86@gmail.com>
2024-02-14 06:24:13 -0500

    python_variable_annotations

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
+"""
+Uses Python type hints to specify that
+'floor' takes in a float, and returns an int.
+"""
+
 
 def floor(n: float) -> int:
+    """
+    Assuming that 'n' is a float,
+    returns the floored version of 'n',
+    which should be an 'int', since 'n'
+    is assumed to be a 'float'.
+    """
     return int(n)
