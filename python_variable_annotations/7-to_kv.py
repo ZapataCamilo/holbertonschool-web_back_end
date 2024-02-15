0d497d18a7e2871f7132b074ebc379fe22995cab
ZapataCamilo <zapatacamilo86@gmail.com>
2024-02-15 09:39:20 -0500

    python_variable_annotations

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,18 @@
+#!/bin/usr/env python3
+"""
+This time, this file uses
+'Tuple' to specify that 'to_kv'
+returns a tuple, and 'Union'
+for the 'v' parameter.
+"""
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Returns a tuple with:
+    'k' as the first item and
+    the float representation
+    of v squared as the second item.
+    """
+    return k, float(v ** 2)
