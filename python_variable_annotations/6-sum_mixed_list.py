03cd381b73e360cf1a119a4db6b8db181b9586f7
ZapataCamilo <zapatacamilo86@gmail.com>
2024-02-15 09:18:08 -0500

    python_variable_annotations

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Has one function named 'sum_mixed_list'.
+
+'sum_mixed_list' takes one argument 'mxd_lst',
+which should be a list of ints or floats,
+and returns their sum, which should be a float.
+"""
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[float]) -> float:
+    """
+    Returns the sum of all of the numbers in 'mxd_list',
+    assuming that 'mxd_list' is a list
+    """
+    return sum(mxd_lst)
