4b39a67b0f00c7c630a7333fd3fc4cca17950c6f
ZapataCamilo <zapatacamilo86@gmail.com>
2024-02-16 08:28:09 -0500

    python_async_function

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+'Hello World!' figue in Python's
+documentation for 'asyncio'.
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Chooses a random number
+    (somewhat) in the range of
+    0 and 'max_delay',
+    using 'random.uniform'.
+    """
+    dl: float = random.uniform(0, max_delay)
+    return dl
