95a478cebe9751f083b014ddfaabb682e911889c
ZapataCamilo <zapatacamilo86@gmail.com>
2024-02-18 20:03:05 -0500

    pagination

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,17 @@
+#!/usi/bin/env python3
+"""
+Write a function named index_range that
+takes two integer arguments page and page_size.
+"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+
+    This function returns the index of the first
+    and last article in that page (0-INDEXED).
+    """
+    START: int = page_size * (page - 1)
+    END: int = START + page_size
+    return (START, END)
